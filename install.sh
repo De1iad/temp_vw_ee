98ada473ba240499c3f1f0933a8c604e09453e72
Oliver Bibby <deliad@macbook-pro.home>
2023-02-26 15:06:17 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,10 @@ then
 else
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-brew install fftw
 brew install SDL2
+wget http://fftw.org/pub/fftw/fftw-3.2.2.tar.gz
+tar -xf fftw-3.2.2.tar.gz
+cd fftw-3.2.2
+./configure
+make
+make install
